928c3ec7e4ed2bb8ac3228092c574c545ed211e9
Muqimjon <muqimjon.mr@gmail.com>
2023-11-19 16:28:02 +0500

    Add project files.

---
+++ DashPod.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DashPod.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
