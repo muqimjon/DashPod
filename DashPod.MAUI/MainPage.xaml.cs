498fb2fa9e1de110d03493300bbbc41db1f2750f
Muqimjon <muqimjon.mr@gmail.com>
2023-11-19 16:34:53 +0500

    Clean crash

--- DashPod.MAUI/MainPage.xaml.cs
+++ DashPod.MAUI/MainPage.xaml.cs
@@ -1,25 +1,5 @@
-﻿namespace DashPod.MAUI
+﻿namespace DashPod.MAUI;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
-    {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
-    }
-
 }
