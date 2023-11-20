4de1eaf304445014292969bd4684beb238a8e64c
Muqimjon <muqimjon.mr@gmail.com>
2023-11-20 19:34:54 +0500

    Changed  item groups

--- DashPod.MAUI/Platforms/Tizen/Main.cs
+++ DashPod.MAUI/Platforms/Tizen/Main.cs
@@ -2,16 +2,15 @@ using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 using System;
 
-namespace DashPod.MAUI
-{
-    internal class Program : MauiApplication
-    {
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+namespace DashPod.MAUI;
 
-        static void Main(string[] args)
-        {
-            var app = new Program();
-            app.Run(args);
-        }
+internal class Program : MauiApplication
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    static void Main(string[] args)
+    {
+        var app = new Program();
+        app.Run(args);
     }
 }
